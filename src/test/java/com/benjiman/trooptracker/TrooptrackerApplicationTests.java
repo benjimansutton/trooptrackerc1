73478fc95d983c5109e2fe5254833d0b84656dbd
Benjiman Sutton <benjimansutton@gmail.com>
2022-09-14 10:42:02 +0100

    first commit

---
+++ src/test/java/com/benjiman/trooptracker/TrooptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjiman.trooptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrooptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
